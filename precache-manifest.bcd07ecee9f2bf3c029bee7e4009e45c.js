2fad4c5da56f97f46c6eff1055d9fd32a8af5a9e
Roush,Kyle <Kyle.Roush@cerner.com>
2020-05-10 14:48:07 -0500

    Updates

---
+++ precache-manifest.bcd07ecee9f2bf3c029bee7e4009e45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105fbde0d4425ef35bda7f98a9cc35e4",
+    "url": "/telestration/index.html"
+  },
+  {
+    "revision": "508b10b0e92103e38c8c",
+    "url": "/telestration/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a3ea75eb21493a1382f4",
+    "url": "/telestration/static/js/2.3ed4c05e.chunk.js"
+  },
+  {
+    "revision": "91ae9c2adf0bf59216f345d552893bfc",
+    "url": "/telestration/static/js/2.3ed4c05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508b10b0e92103e38c8c",
+    "url": "/telestration/static/js/main.5bf0e5e2.chunk.js"
+  },
+  {
+    "revision": "ee3f418446474f74a428",
+    "url": "/telestration/static/js/runtime-main.de9c7e1f.js"
+  }
+]);
